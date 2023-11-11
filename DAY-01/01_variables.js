606a0fea659f910fed28598e96238667d829eb44
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-11 17:27:01 +0000

    let var const and their scope ,declaration and update

--- DAY-01/01_variables.js
+++ DAY-01/01_variables.js
@@ -1,5 +1,23 @@
 console.log("hello");
 // let const var
-let userName="Radhakant Panda";
+let userName="Radhakant Panda";//sinu
 userName="sinu";//  variable  overwritting 
-console.log(userName);
+console.log(userName);//
+
+
+
+// *********************** let , var and const ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let Name="Radhakant";
+{
+    let Name="sinu";
+    console.log(Name);// sinu
+}
+console.log(Name);//Radhakant
+
+
+var userID="radhakant@google";
+{
+    var userID="sinu@ytb";// var is  not block scoped 
+}
+console.log(userID);// sinu@ytb
