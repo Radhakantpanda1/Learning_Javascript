634b8841efff46b7242605552f373bc9fa0f377c
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-11 18:12:25 +0000

    practice setsolutions

---
+++ DAY-01/01_practice_set_solution.js
@@ -0,0 +1,8 @@
+// 1. Create a variable of type string and try to add a number to it.
+console.log("radhakant "+ 2002);
+let container="radhakant"+25;
+console.log(container);
+
+//2.Use typeof operator to find the data type of above question.
+console.log(typeof(container));// string
+
