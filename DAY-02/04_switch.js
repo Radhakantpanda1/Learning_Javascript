ac804f27e2f4c4ae512c72602b294479066221c6
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-12 14:13:11 +0000

    switch case in JS

---
+++ DAY-02/04_switch.js
@@ -0,0 +1,16 @@
+// switch case 
+let fruit="mangoes";
+switch (fruit) {
+    case "mangoes": alert("price is 200rs per kg");
+           break;
+    case "banana": alert("price is 181rs per kg");
+           break;
+    case "guava": alert("price is 180rs per kg");
+           break;
+    case "avocado": alert("price is 1200rs per kg");
+           break;
+
+    default: alert(`we are sorry we don't have ${fruit}`);
+        break;
+}
+// run at console to know how it works
