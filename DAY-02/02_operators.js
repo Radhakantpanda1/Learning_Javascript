463b301ad47ca55979ab95868598f6b88c35b117
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-12 03:39:40 +0000

    operators-logical,assignment,comparision

---
+++ DAY-02/02_operators.js
@@ -0,0 +1,27 @@
+let x=5;
+let y=3;
+/*
+console.log("x+y=",(x+y));
+console.log("x-y=",(x-y));
+console.log("x*y=",(x*y));
+console.log("x/y=",(x/y));
+console.log("x**y=",(x**y));
+console.log("x%y=",(x%y));
+*/
+/*
+let a=235;
+console.log(++a);
+console.log(--a);
+console.log(a--);
+console.log(++a);
+console.log(a--);
+*/
+const k=2;
+/*
+console.log(++k);
+console.log(--k);
+console.log(k--);
+console.log(++k);
+console.log(k--);
+*/
+// output- Assignment to constant variable.
