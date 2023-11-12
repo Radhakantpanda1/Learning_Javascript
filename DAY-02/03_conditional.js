ca28379e983951e3a02c42f5c724235a67f31bf5
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-12 13:55:16 +0000

    conditional in JS

---
+++ DAY-02/03_conditional.js
@@ -0,0 +1,18 @@
+
+/*let age=prompt("enter your age");
+age=Number.parseInt(age);
+if(age<18){
+    alert("Underage");
+}
+else{
+    alert("You van proceed further");
+}
+// run this code in console to know how it works.
+
+*/
+// HW- Explore switch case by yourself
+
+// ternary operator
+
+let userAge=25;
+console.log((userAge>18)? "you can vote":"you can't vote");
