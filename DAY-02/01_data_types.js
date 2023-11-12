720a4b4d1fc13b0cafdb2a1a85ae2fce3e130afb
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-12 03:38:59 +0000

    data_types_primitive_and_object

---
+++ DAY-02/01_data_types.js
@@ -0,0 +1,45 @@
+// nn bb ss u
+let a=25;
+let b=null;
+let c= true;
+let d= BigInt("1200");
+let e="string";
+let f=Symbol("i am s symbol");
+let g= undefined;
+/*
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(typeof f);
+*/
+
+// quiz solution-01
+/*
+let userDetails={
+    userName:"Radhakant Panda",
+    userMobleNumber:BigInt("94369541")
+}
+console.log(userDetails);
+*/
+
+// Quiz solution-02
+/*
+let dictionary={
+    delusion:"a false belief or judgment about external reality",
+    dupe:"a victim of deception",
+    "gull":"a person who is fooled or deceived",
+    simpleton:"a foolish or gullible person",
+    ignoramus:"an ignorant or stupid person"
+}
+console.log(typeof dictionary);// object
+console.log(typeof dictionary.delusion);// string
+console.log( dictionary["delusion"]);//a false belief or judgment about external reality
+console.log( dictionary["gull"]);//a person who is fooled or deceived
+
+dictionary.dupe="lk";
+console.log(dictionary["dupe"]);
+*/
