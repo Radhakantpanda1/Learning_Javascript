4c018b1d8eedf9d9153b48f89d0dc6db8a23f0ad
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-13 07:53:32 +0000

    understanding functions , arrow functions

---
+++ DAY-03/03_function.js
@@ -0,0 +1,19 @@
+//  function
+// a block of code which performs a particular task whenever called
+
+// function to find sum of two numbers
+function sum(a , b) {
+    return a+b;
+}
+console.log(sum(2,7));
+
+
+// arrow function
+ let sumoftwo= (x,y) => x+y;
+ console.log(sumoftwo(1,5)); // implicitly defined return 
+
+ //
+let sumofthree=(x,y,z)=>{
+    return x+y+z;
+}
+console.log(sumofthree(1,5,9));
