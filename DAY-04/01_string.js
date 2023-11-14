6a6eaf173f896716ed3b1e395eeb18dcf53824e5
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-14 13:00:36 +0000

    basic strings

---
+++ DAY-04/01_string.js
@@ -0,0 +1,10 @@
+let userName="Radhakant Panda";
+console.log(userName);
+console.log(userName.length);
+console.log(userName[0]);
+console.log(userName[1]);
+console.log(userName[5]);
+console.log(userName[7]);
+console.log(`Username is ${userName}`);
+
+// String interpolation-We can insert variables directly in a template literal.
