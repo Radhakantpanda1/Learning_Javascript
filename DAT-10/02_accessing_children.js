35fadc67f461cedbf549df4888290b1a357bbd2c
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-21 02:14:41 +0000

    accessing the children

---
+++ DAT-10/02_accessing_children.js
@@ -0,0 +1,11 @@
+// child nodes- element that are direct child for rexample head and body are direct child of html
+// descendanr nodes-all nested elements their children and so on
+
+// first child , last child and child nodes
+// document.element.firstChild;
+// document.element.lastChild;
+// document.element.childNodes; list of all child nodes of that element
+
+// elem.hasChildNodes()  to check whether the element has child nodes or not.
+
+// note document.element.childNodes; is note an array it is an object but you can convert it to an array using Array.form().
